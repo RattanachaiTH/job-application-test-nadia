f19fd9b4a3d7c6987bb4ed3b60dc32e5adc28d6d
SuchakreeS <maxsomonkey@gmail.com>
2019-03-01 20:47:50 +0700

    Final FileSystem

---
+++ FileSystem/Assets/Scripts/FileManager.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+using TMPro;
+using System.Windows.Forms;
+
+public class FileManager : MonoBehaviour
+{
+    // Initial public variable
+    public static int captureHeight = 2048;
+    public static int captureWidth = 2048;
+    public GameObject imageObject;
+    public GameObject toggleVersion;
+    public GameObject pathObject;
+    public GameObject lastSavedObject;
+    public Camera cam;
+
+    // Initial private variable
+    private string defaultPath;
+    private string path;
+    private string prefixName;
+    private string fileType;
+    private int imageCount;
+    private Image image;
+    private Toggle versionToggle;
+    private bool save;
+    private bool selectPath;
+    private bool version;
+    private string[] allowType;
+    HashSet<string> currentImageFiles;
+
+    void Awake()
+    {
+        // initial variable
+        defaultPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+        path = defaultPath;
+        prefixName = "IOsystem_Image";
+        fileType = "png";
+        image = imageObject.transform.GetComponent<Image>();
+        versionToggle = toggleVersion.GetComponent<Toggle>();
+        version = false;
+        save = false;
+        selectPath = false;
+        imageCount = 0;
+        allowType = new string[] { ".png", ".jpg", ".PNG", ".JPG" };
+
+        // Check files in folder
+        currentImageFiles = ImageFilesSetFromPath(path);
+    }
+
+    void Update()
+    {
+        CheckDropedFile(path);
+        CheckToggle();
+        CheckInput();
+        Procressing();
+    }
+
+    // Check Input
+    void CheckInput()
+    {
+        if (Input.GetButtonDown("Enter"))
+        {
+            save = true;
+        }
+        if (Input.GetButtonDown("Space"))
+        {
+            selectPath = true;
+        }
+        pathObject.transform.GetComponent<TextMeshProUGUI>().SetText("Selected path      : " + path);
+    }
+
+    // Option in application
+    void Procressing()
+    {
+        // When press "Enter"
+        if (save == true)
+        {
+            CaptureScreen();
+            save = false;
+        }
+        // When press "Enter"
+        else if (selectPath == true)
+        {
+            //string newPath = EditorUtility.OpenFolderPanel("Select new Path", path, "");
+            OpenFileDialog folderDialog = new OpenFileDialog();
+            folderDialog.ValidateNames = false;
+            folderDialog.CheckFileExists = false;
+            folderDialog.CheckPathExists = true;
+            folderDialog.FileName = "Folder Selection";
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                string newPath = Path.GetDirectoryName(folderDialog.FileName);
+                print("Path: " + newPath);
+                path = newPath;
+                CaptureScreen();
+                currentImageFiles = ImageFilesSetFromPath(path);
+            }
+            selectPath = false;
+        }
+    }
+
+    // CaptureScreen in application
+    void CaptureScreen()
+    {
+        imageCount++;
+        // Capturing
+        RenderTexture render = new RenderTexture(captureWidth, captureHeight, 24);
+        cam.targetTexture = render;
+        Texture2D screenShot = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
+        cam.Render();
+        RenderTexture.active = render;
+        screenShot.ReadPixels(new Rect(0, 0, captureWidth, captureHeight), 0, 0);
+        cam.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(render);
+
+        // Saving image
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filePath = GetCapturePath();
+        System.IO.File.WriteAllBytes(filePath, bytes);
+        versionToggle.isOn = version;
+
+        // show image from path in UI
+        ShowImageOnScreen(filePath);
+        lastSavedObject.transform.GetComponent<TextMeshProUGUI>().SetText("Last saved image: " + filePath);
+
+        // Show saved image path
+        Debug.Log(string.Format("Saved: {0}", filePath));
+    }
+
+    // Get full path for saving file
+    string GetCapturePath()
+    {
+        if (version == true)
+        {
+            return string.Format("{0}/{1}_{2}.{3}", path, prefixName, imageCount, fileType);
+        }
+        else
+        {
+            return string.Format("{0}/{1}.{2}", path, prefixName, fileType);
+        }
+    }
+
+    // Get Texture2d from image path
+    Texture2D LoadImageFromPath(string filePath)
+    {
+        Texture2D image;
+        byte[] data;
+        if (File.Exists(filePath))
+        {
+            data = File.ReadAllBytes(filePath);
+            image = new Texture2D(captureWidth, captureHeight);
+            image.LoadImage(data);
+            return image;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    // Get image file name list from path
+    HashSet<string> ImageFilesSetFromPath(string path)
+    {
+        // Get files from directory
+        DirectoryInfo directory = new DirectoryInfo(path);
+        FileInfo[] files = directory.GetFiles("*.*");
+        HashSet<string> filePathSet = new HashSet<string>();
+
+        // Check Image file
+        foreach (FileInfo file in files)
+        {
+            foreach (string type in allowType)
+            {
+                if (file.FullName.EndsWith(type))
+                {
+                    filePathSet.Add(file.FullName);
+                    break;
+                }
+            }
+        }
+        return filePathSet;
+    }
+
+    // Check new tem that dropped in selected path
+    HashSet<string> CheckNewItem(HashSet<string> oldItem, HashSet<string> newItem)
+    {
+        // clone newItem
+        HashSet<string> newItemClone = CloneHashSet(newItem);
+
+        // check new item
+        newItemClone.ExceptWith(oldItem);
+        return newItemClone;
+    }
+
+    // Clone hashset 
+    HashSet<string> CloneHashSet(HashSet<string> set)
+    {
+        // clone newItem
+        HashSet<string> clone = new HashSet<string>();
+        foreach (string item in set)
+        {
+            clone.Add(item);
+        }
+        return clone;
+    }
+
+    // Check dropped file
+    void CheckDropedFile(string path)
+    {
+        HashSet<string> nextImageFiles = ImageFilesSetFromPath(path);
+        HashSet<string> newItemSet = CheckNewItem(currentImageFiles, nextImageFiles);
+        if (newItemSet.Count != 0)
+        {
+            List<string> itemList = new List<string>(newItemSet);
+            foreach (string fullPath in itemList)
+            {
+                ShowImageOnScreen(fullPath);
+            }
+        }
+        currentImageFiles = nextImageFiles;
+    }
+
+    // Show new item on screen
+    void ShowImageOnScreen(string filePath)
+    {
+        Texture2D text = LoadImageFromPath(filePath);
+        text = TextureResize(text, captureWidth, captureHeight);
+        Sprite savedImageSprite = Sprite.Create(text, new Rect(0.0f, 0.0f, captureWidth, captureHeight), new Vector2(0.5f, 0.5f), 100.0f);
+        image.sprite = savedImageSprite;
+    }
+
+    // Resize image size to texture
+    Texture2D TextureResize(Texture2D source, int width, int height)
+    {
+        source.filterMode = FilterMode.Point;
+        RenderTexture rend = RenderTexture.GetTemporary(width, height);
+        rend.filterMode = FilterMode.Point;
+        RenderTexture.active = rend;
+        Graphics.Blit(source, rend);
+        Texture2D newTexture = new Texture2D(width, height);
+        newTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        newTexture.Apply();
+        RenderTexture.active = null;
+        return newTexture;
+
+    }
+    
+    // Check press toggle
+    void CheckToggle()
+    {
+        if (versionToggle.isOn == true)
+        {
+            version = true;
+        }
+        else
+        {
+            version = false;
+        }
+    }
+}
